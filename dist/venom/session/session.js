6f15b239140a835176e330593cad7522096e6247
Rafael <rafaelflorianipinto@gmail.com>
2022-04-10 12:42:05 -0300

    TSC

--- dist/venom/session/session.js
+++ dist/venom/session/session.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getVenomSessionName = void 0;
 function getVenomSessionName(userId, sessionNumber) {
-    return `UserId ${userId} Bot ${sessionNumber}`;
+    return `UserId_${userId}_Bot_${sessionNumber}`;
 }
 exports.getVenomSessionName = getVenomSessionName;
